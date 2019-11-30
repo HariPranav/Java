e947d0e4ded267f940dbabf4f6a8597842c08c6d
Hari Pranav <haripranav98@gmail.com>
2019-11-30 07:17:13 +0530

    Create RotationOfStrings.java

---
+++ RotationOfStrings.java
@@ -0,0 +1,23 @@
+// Program to find whether two strings are the rotation of each other
+
+// The logic is that we need to find the concatenation of str1 with str1 and then we need to find whether the str2 length is same as str1 and then
+//we need to find the whether the str2 is the substring of concat of str1+str1
+
+
+// eg : str1  = ABCD
+// Concat = ABCDABCD
+// str2= DABC
+/* main()
+  a=str1.length();
+  b=str2.length();
+  if(Check(s,a))
+  
+  Sopln(Yes it is a substring)
+  
+  Check(String s,String a)
+  {
+    if(a==b && (str1+str1).charAt[i]==str2.charAt[i])
+    return 1;
+  }
+  
+*/
